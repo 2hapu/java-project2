61666a33e15f9a25c05cc9c28cdab61bbedf868f
2hapu <yonzn@naver.com>
2023-04-20 15:23:37 +0900

    feat: 실수형, 변수 실습 추가

---
+++ src/main/java/com/example/javaproject/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,12 @@
+package com.example.javaproject.week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+
+//        byte byte2 = -129;
+//        byte byte3 = 128;
+
+        System.out.println(byte1);
+    }
+}
---
+++ src/main/java/com/example/javaproject/week1/day4/DefineAndInitialize.java
@@ -0,0 +1,8 @@
+package com.example.javaproject.week1.day4;
+
+import com.example.javaproject.PrintHello;
+
+public class DefineAndInitialize {
+    int iVal = 1;
+    PrintHello printHello = new PrintHello();
+}
