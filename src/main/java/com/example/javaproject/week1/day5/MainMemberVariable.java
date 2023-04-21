5c51014d9f27df9195b9a798441f8b7ef543aee0
2hapu <yonzn@naver.com>
2023-04-21 14:13:35 +0900

    feat: 멤버 변수 활용하여 SpaceInvadersMemberVariable 실습

---
+++ src/main/java/com/example/javaproject/week1/day5/MainMemberVariable.java
@@ -0,0 +1,9 @@
+package com.example.javaproject.week1.day5;
+
+public class MainMemberVariable {
+    static int location;
+
+    public static void main(String[] args) {
+        System.out.println(location);
+    }
+}
--- src/main/java/com/example/javaproject/week1/day5/SpaceInvadersMemberVariable.java
+++ src/main/java/com/example/javaproject/week1/day5/SpaceInvadersMemberVariable.java
@@ -4,11 +4,9 @@ public class SpaceInvadersMemberVariable {
     int location; // 맴버 변수 (class의 멤버다)
     public void moveLeft() {
         location = location - 1;
-        System.out.printf("moveLeft : %d\n", location);
     }
     public void moveRight() {
         location = location + 1;
-        System.out.printf("moveRight : %d\n", location);
     }
 
     public static void main(String[] args) {
@@ -18,5 +16,9 @@ public class SpaceInvadersMemberVariable {
         simv.moveRight();
         simv.moveRight();
         simv.moveRight();
+
+        System.out.printf("최종 위치: %d\n",simv.location);
+        simv.moveLeft();
+        System.out.printf("최종 위치: %d\n",simv.location);
     }
 }
