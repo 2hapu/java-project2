b45cf23514913dcfac1febceb050bdb3958a7b88
2hapu <yonzn@naver.com>
2023-04-26 15:38:39 +0900

    feat: 변수에 값 누적 BankAccount, LikeAccumulate 추가

---
+++ src/main/java/com/example/javaproject/week2/day3/BankAccount.java
@@ -0,0 +1,14 @@
+package com.example.javaproject.week2.day3;
+
+public class BankAccount {
+    public static void main(String[] args) {
+        int myAccount = 5_000_000;
+        System.out.println(myAccount);
+
+        myAccount = myAccount - 600_000;
+        System.out.println(myAccount);
+
+        myAccount = myAccount + 300_000;
+        System.out.println(myAccount);
+    }
+}
