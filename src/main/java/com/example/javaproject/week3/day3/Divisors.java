98a803c15b9985bc35b7915d026babbf3dd2f691
2hapu <yonzn@naver.com>
2023-05-03 11:36:37 +0900

    fix: programmers, divisors

--- src/main/java/com/example/javaproject/week3/day3/Divisors.java
+++ src/main/java/com/example/javaproject/week3/day3/Divisors.java
@@ -1,4 +1,17 @@
 package com.example.javaproject.week3.day3;
 
+import java.util.Scanner;
+
 public class Divisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
 }
