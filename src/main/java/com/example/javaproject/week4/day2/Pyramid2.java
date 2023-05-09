efd2e09f1d02d4c823a426fc6541209baca5ce3a
2hapu <yonzn@naver.com>
2023-05-09 09:31:13 +0900

    feat: pyramid2

---
+++ src/main/java/com/example/javaproject/week4/day2/Pyramid2.java
@@ -0,0 +1,22 @@
+package com.example.javaproject.week4.day2;
+
+public class Pyramid2 {
+    private String blankChar;
+
+
+    public Pyramid2(String blankChar) {
+        this.blankChar = blankChar;
+    }
+
+    public void printPyramid(int height) {
+        for (int i = 0; i < height; i++) {
+            System.out.printf("%s%s\n", blankChar.repeat(height - i - 1), "*".repeat(2 * i + 1));
+        }
+    }
+    public static void main(String[] args) {
+        Pyramid2 pyramid2 = new Pyramid2(" "); // argument " "를 넘겨서 초기화
+        Pyramid2 pyramidSpaceZero = new Pyramid2("0"); // argument "0"을 넘겨서 초기화
+        pyramid2.printPyramid(4);
+        pyramidSpaceZero.printPyramid(5);
+    }
+}
